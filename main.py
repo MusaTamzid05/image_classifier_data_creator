1469fdd1b5805ab2f8a067f2d58928567ed2c42c
MusaTamzid05 <tamzid101@gmail.com>
2021-10-09 14:34:32 +0600

    image_size is npw in arg parse.

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", dest = "save_dir", type = str, required = True)
     parser.add_argument("-c", dest = "count", type = int)
+    parser.add_argument("-is", dest = "image_size", type = int, default = 224)
 
     args = parser.parse_args()
 
-    data_creator = CreateData(max_count = args.count)
+    data_creator = CreateData(max_count = args.count, image_size =  args.image_size)
     total_count = data_creator.run(save_path_dir = args.save_dir)
 
     print(f"Total image {total_count} saved.")
