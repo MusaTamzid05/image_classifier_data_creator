1517aabf7c9ecee2296c8013753b1beada8d1d9e
MusaTamzid05 <tamzid101@gmail.com>
2021-10-09 14:30:33 +0600

    argparse added.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import argparse
 from lib.data_creator import CreateData
 
 def main():
-    data_creator = CreateData()
-    data_creator.run(save_path_dir = "./test")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", dest = "save_dir", type = str, required = True)
+    parser.add_argument("-c", dest = "count", type = int)
+
+    args = parser.parse_args()
+
+    data_creator = CreateData(max_count = args.count)
+    total_count = data_creator.run(save_path_dir = args.save_dir)
+
+    print(f"Total image {total_count} saved.")
 
 if __name__ == "__main__":
     main()
